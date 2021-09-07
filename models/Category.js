e1666b1ab0bce00027a286943e9e27b04ea7338d
Steve C <stevencarofano@gmail.com>
2021-09-07 12:25:22 -0400

    Setting up connections

--- models/Category.js
+++ models/Category.js
@@ -6,7 +6,17 @@ class Category extends Model {}
 
 Category.init(
   {
-    // define columns
+    // Defining id and category names for DB columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    category_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
   },
   {
     sequelize,
